832802a0191acf5e011063d66d97761055e1a64a
jardel3kl <jardel3kl@gmail.com>
2025-01-22 10:35:48 -0400

    teste 10

--- src/routes/pagina_inicial/+layout.svelte
+++ src/routes/pagina_inicial/+layout.svelte
@@ -19,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="/sobre">Sobre</a>
+                    <a class="nav-link" href="/filme">filme</a>
                 </li>
             </ul>
         </div>
