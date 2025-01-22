a04db7dc78764091420822bd33a4d0803327afea
jardel3kl <jardel3kl@gmail.com>
2025-01-21 21:46:48 -0400

    coloquei uma barra de navegação

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<style>
-body {
-    background-image: url("https://uploads.metroimg.com/wp-content/uploads/2022/10/20172225/240417DF-lula-em-brasilia-032-1.jpg");
-  }
-</style>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="youtube.com">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div></nav>
