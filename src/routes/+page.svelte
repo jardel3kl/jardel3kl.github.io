b37b9d21c30f1f9688ada9822a0fe6ddc6afd90f
jardel3kl <jardel3kl@gmail.com>
2025-01-22 09:18:25 -0400

    teste 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar com Bootstrap</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="youtbe.com">MeuSite</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="https://pedrosiqueira.github.io/ifmsjs/dw4/">Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://jardel3kl.github.io/">Sobre</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://ead.ifms.edu.br/">Serviços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://www.whatsapp.com/?lang=pt_BR">Contato</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<script>
+    let a = 10
+    let b = 1000
+</script>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<p>{a}</p>
