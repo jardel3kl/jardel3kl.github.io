86208d726226a05af8ca9ea17086474d1e613302
jardel3kl <jardel3kl@gmail.com>
2025-01-21 21:57:24 -0400

    nada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <!-- Só uma imagem -->
 <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="youtube.com">
       <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
     </a>
   </nav>
