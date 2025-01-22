174e47b82599ffd9ea2eb0ed825d9e68d87be51b
jardel3kl <jardel3kl@gmail.com>
2025-01-21 22:16:00 -0400

    fibonati

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,104 +1,41 @@
 <script>
-    import { filmes, gêneros } from '$lib/filmes.js';
-  
-    let gênerosSelecionados = $state([]);
-    let filtrados = $state(filmes.slice());
-    let busca = $state('');
-  
-    function filtrartitulo(event) {
-      console.log(busca);
-      //  se o campo de busca estiver vazio nao faz nada e encerre a funcão comando return
-      if (busca == '') {
-        filtrados = filmes.slice();
-        return;
-      }
-      filtrados = [];
-      //pra cada filme dos meus filmes
-      for (const filme of filmes) {
-        //  se este o titulo deste filme conter o campo de busca
-        if (filme.título.includes(busca)) {
-          //    adicione este filme a lista de filmes filtrados
-          filtrados.push(filme);
-          //  senao
-        } else {
-          //    exclua este filme da lista de filtrados
-        }
-      }
+    let fibonacciNumbers = [0, 1];
+    let currentMonth = 0;
+
+    function increaseFibonacciNumbers() {
+        const nextNumber = fibonacciNumbers[fibonacciNumbers.length - 1] + fibonacciNumbers[fibonacciNumbers.length - 2];
+        fibonacciNumbers.push(nextNumber);
+        currentMonth++;
     }
-  
-    function filtrarGenero(event) {
-      if (event.target.checked) {
-        gênerosSelecionados.push(event.target.value);
-      } else {
-        gênerosSelecionados.splice(gênerosSelecionados.indexOf(event.target.value), 1);
-      }
-  
-      if (gênerosSelecionados.length == 0) {
-        filtrados = filmes.slice();
-      } else {
-        filtrados = [];
-        for (const filme of filmes) {
-          for (const gênero of gênerosSelecionados) {
-            if (filme.gêneros.includes(gênero)) {
-              filtrados.push(filme);
-              break;
-            }
-          }
+
+    function decreaseFibonacciNumbers() {
+        if (currentMonth > 0) {
+            currentMonth--;
+            fibonacciNumbers.pop();
         }
-      }
     }
-  </script>
-  
-  <div class="row align-items-center mb-3">
-    <div class="col">
-      <input
-        bind:value={busca}
-        oninput={filtrartitulo}
-        class="form-control"
-        placeholder="Filtrar..."
-      />
-    </div>
-    {#each gêneros as gênero}
-      <div class="col">
-        <div class="form-check form-check-inline">
-          <input
-            oninput={filtrarGenero}
-            class="form-check-input"
-            type="checkbox"
-            id={gênero}
-            value={gênero}
-          />
-          <label class="form-check-label" for={gênero}>{gênero}</label>
-        </div>
-      </div>
-    {/each}
-  </div>
-  <div class="row g-4">
-    {#each filtrados as filme}
-      <div class="col-md-6 col-xl-3">
-        <div class="card h-100">
-          <div class="row g-0">
-            <div class="col-3 col-sm-4">
-              <img src={filme.imagem} class="img-fluid rounded-start" alt="capa do filme" />
-              <a target="_blank" href={filme.referência} class="btn btn-outline-dark">Referências</a>
-            </div>
-            <div class="col-sm-8">
-              <div class="card-body">
-                <h5 class="card-title">{filme.título}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">{filme.ano}</h6>
-                <p class="card-text">{filme.sinopse}</p>
-                <p class="card-text">
-                  
-                  {#each filme.gêneros as gênero}
-                    <span class="badge text-bg-secondary mx-1">{gênero}</span>
-                  {/each}
-  
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    {/each}
-  </div>
-  
+
+    function changeMonth(event) {
+        currentMonth = parseInt(event.target.value);
+        while (currentMonth >= fibonacciNumbers.length) {
+            const nextNumber = fibonacciNumbers[fibonacciNumbers.length - 1] + fibonacciNumbers[fibonacciNumbers.length - 2];
+            fibonacciNumbers.push(nextNumber);
+        }
+    }
+</script>
+
+<h1>Os Coelhos de Fibonacci</h1>
+
+<p>
+    Mês:
+    <input 
+        type="number" 
+        min="0" 
+        value={currentMonth} 
+        on:input={changeMonth} 
+    />
+    <button on:click={increaseFibonacciNumbers}>Próximo mês</button>
+    <button on:click={decreaseFibonacciNumbers}>Mês anterior</button>
+</p>
+
+<p>Após {currentMonth} meses, haverá {fibonacciNumbers[currentMonth]} ninhadas de coelhos!</p>
