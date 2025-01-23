8d2b4cde6605e68b6d107d0681ed15ba9b452976
jardel3kl <jardel3kl@gmail.com>
2025-01-23 08:57:20 -0400

    teste 25

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href=/src/routes/fibonati/+page.svelte>
-      <img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTExL3YxMTgxLXR1LWVsZW1lbnQtMjUwLXAtbTN3cXR3dTEucG5n.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      Home
-    </a>
+    <a class="navbar-brand" href="/fibonati">fibonati</a>
+    <img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTExL3YxMTgxLXR1LWVsZW1lbnQtMjUwLXAtbTN3cXR3dTEucG5n.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
   </div>
 </nav>
