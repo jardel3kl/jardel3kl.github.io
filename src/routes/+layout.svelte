093e2ba11e34537e95b52b63090e1b61423d11e9
jardel3kl <jardel3kl@gmail.com>
2025-01-23 11:59:22 -0400

    teste 31

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,10 +12,6 @@ let { children } = $props();
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/carrinho">Carrinho</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/fibonati">Fibonacci</a>
         </li>
       </ul>
@@ -23,9 +19,13 @@ let { children } = $props();
   </div>
 </nav>
 
-
-
-
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="/carrinho">Carrinho</a>
+    </li>
+  </ul>
+</div>
 
 
 
