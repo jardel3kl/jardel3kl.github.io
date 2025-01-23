98dd188a40927fd997466d0091c198bb616196ef
jardel3kl <jardel3kl@gmail.com>
2025-01-23 08:31:45 -0400

    top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,7 @@
-<script>
-    let { children } = $props();
-</script>
-
-<nav class="navbar navbar-expand-sm bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/#">DW4</a>
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/sobre">Sobre</a>
-                </li>
-            </ul>
-        </div>
+<nav class="navbar bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="/cartas">
+        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+      </a>
     </div>
-</nav>
-
-{@render children()}
+  </nav>
